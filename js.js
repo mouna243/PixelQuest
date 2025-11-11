574f589207941f5f7d77a9664f3f118b64005bfa
Mouna Saoui <mounasaoui2108@gmail.com>
2025-11-11 15:39:19 +0100

    la page Explore (#23)
    * l'ajoute de respensive
    
    * le fetch et affichage des API

--- js.js
+++ js.js
@@ -2,19 +2,20 @@
  const container = document.querySelector(".carts")
 async function  loadData (){
     try{
-    const url = await fetch('https://debuggers-games-api.duckdns.org/api/games?page=2&limit=1');
+    const url = await fetch('https://debuggers-games-api.duckdns.org/api/games?page=2&limit=5');
     let data  = await url.json() ;
 
     console.log(data.results) ;
     data.results.forEach((result) =>{
        const cart = document.createElement('div');
        console.log(cart)
-       const description = result.description ;
-       const titre = result.name ;
-       const image = result.background_image_additional;
-       const date = result.released ;
-       const studio = result.platforme ;
-       cart.innerHTML=(` <div class="cart w-[288px] h-[386px]">
+       const description = result.description || 'pas de result';
+       const titre = result.name || 'pas de result';
+       const url = result.background_image_additional|| 'pas de result';
+       const date = result.released || 'pas de result';
+       const studio = result.developers.name || 'pas de result' ;
+       const rating = result.rating || 'pas de result';
+       cart.innerHTML=( `<div class="cart w-[288px] h-[386px]">
         <img src="${image}" alt="${titre} class="w-[288px] h-[386px]"">
         <div>
         <h2 class="bg-red-500">${titre}</h2>
